07d41c530691cfc94393bc0eefcbd8e15b17abb7
fsusuki <foosoo200@gmail.com>
2023-07-25 08:37:43 +0900

    Project Start

---
+++ project/database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,51 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('serial_user')->unique();
+            //$table->string('name');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->string('name_sei',100)->comment('姓');
+			$table->string('name_mei',100)->comment('名');
+			$table->string('name_sei_kana',100)->comment('セイ');
+			$table->string('name_mei_kana',100)->comment('メイ');
+			$table->string('gender',10)->nullable();
+			$table->string('birth_year',10)->nullable();
+			$table->string('birth_month',10)->nullable();
+			$table->string('birth_day',10)->nullable();
+			$table->string('postal',15)->nullable();
+			$table->string('address_region')->nullable();
+			$table->string('address_locality')->nullable();
+			$table->string('address_banti')->nullable();
+            $table->string('rank',10)->comment('管理者ランク');
+
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+};
---
+++ project/database/seeders/DatabaseSeeder.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\DB;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //\App\Models\User::factory(10)->create();
+
+        //\App\Models\User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+
+        $init_users = [
+			[
+			'serial_user'=> "T_0000",
+			'email' => "foosoo200@gmail.com",
+			'password' => "0000",
+			'last_name_kanji' => "鈴木",
+			'first_name_kanji' => "文彦",
+			'last_name_jp_kana' => "スズキ",
+			'first_name_jp_kana' => "フミヒコ",
+			'rank' => "A",
+			//'last_name_eng'=> "Suzuki",
+			//'first_name_eng'=> "Fumihiko",
+			//'phone'=> "000-0000-0000",
+			//'address'=> "千葉県************",
+            ]
+        ];
+
+        foreach($init_users as $init_user) {
+			$user = new User();
+			$user->serial_user=$init_user['serial_user'];
+            $user->email = $init_user['email'];
+            $user->password = Hash::make($init_user['password']);
+			$user->name_sei = $init_user['last_name_kanji'];
+			$user->name_mei = $init_user['first_name_kanji'];
+			$user->name_sei_kana = $init_user['last_name_jp_kana'];
+			$user->name_mei_kana = $init_user['first_name_jp_kana'];
+			$user->rank = $init_user['rank'];
+			//$user->rank = $init_user['last_name_eng'];
+				
+			//$user->phone = $init_user['phone'];
+			//$user->address = $init_user['address'];
+			
+			$user->save();
+		}
+
+
+    }
+}
